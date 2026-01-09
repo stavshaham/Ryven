4bac3c228beae5583186f4197af4ba9f7ae17b39
Stav Shaham <shahamstav@gmail.com>
2026-01-09 08:13:12 -0700

    Add email and database configuration variables

--- backend/variables.py
+++ backend/variables.py
@@ -1,1 +1,8 @@
+sender = 'example@domain.com'
+receiver = 'example@domain.com'
+password = 'password'
 
+db_host = 'dbhost'
+db_username = 'dbusername'
+db_password = 'dbpassword'
+db_database = 'dbname'
