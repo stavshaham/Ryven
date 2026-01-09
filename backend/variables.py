0fa8efa9559a34b4f438f46fe328802c2b5a7d3a
stavshaham <shahamstav@gmail.com>
2026-01-09 08:38:25 -0700

    Added variables.py configuration

---
+++ backend/variables.py
@@ -0,0 +1,8 @@
+sender = 'svshdevelopment@gmail.com'
+receiver = 'svshdevelopment@gmail.com'
+password = 'tysq sgxv yisj fdmy'
+
+db_host = 'localhost'
+db_username = 'root'
+db_password = 'Stav2310'
+db_database = 'ryven'
